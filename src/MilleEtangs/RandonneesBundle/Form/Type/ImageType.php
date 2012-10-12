5a376fbc64d0edab838354477d1dcc8eca359a42
maat8 <tuaillon.mathieu@gmail.com>
2012-10-12 21:10:45 +0200

    images upload

---
+++ src/MilleEtangs/RandonneesBundle/Form/Type/ImageType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MilleEtangs\RandonneesBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ImageType extends AbstractType
+{
+	const CUSTOMER_CLASS = 'MilleEtangs\RandonneesBundle\Entity\Image';
+
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('nom')
+			->add('file')
+        ;
+	}
+
+	public function setDefaultOptions(OptionsResolverInterface $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => self::CUSTOMER_CLASS
+		));
+	}
+
+	public function getName()
+	{
+		return 'image';
+	}
+}
