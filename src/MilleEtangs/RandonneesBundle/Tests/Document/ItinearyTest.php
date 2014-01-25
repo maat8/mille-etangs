254f2ef319dc78d5428bec4fcd9b6dcc98b1242a
maat8 <tuaillon.mathieu@gmail.com>
2014-01-25 22:27:49 +0100

    Adding PHPUnit

---
+++ src/MilleEtangs/RandonneesBundle/Tests/Document/ItinearyTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MilleEtangs\RandonneesBundle\Tests\Document;
+
+use MilleEtangs\RandonneesBundle\Document\Itineary;
+
+class ItinearyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+    }
+
+    public function testRetrieve()
+    {
+    }
+
+    public function testUpdate()
+    {
+    }
+
+    public function testDelete()
+    {
+    }
+}
