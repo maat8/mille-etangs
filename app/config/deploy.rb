4ff04d87f528083ae3fb841034be2c3b51638edb
maat8 <tuaillon.mathieu@gmail.com>
2014-02-18 15:32:16 +0100

    Travis CI update

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,9 @@
-set :application, "randos1000etangs"
+set :application, "randos.maat8"
 set :domain,      "#{application}.fr"
 set :deploy_to,   "/srv/www/#{domain}"
 set :app_path,    "public_html"
 
-set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :repository,  "https://github.com/maat8/mille-etangs.git"
 set :scm,         :git
 
 set :model_manager, "doctrine"
@@ -12,6 +12,11 @@ role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
 set  :keep_releases,  3
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+set :use_composer, true
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
