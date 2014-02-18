d0138f22e7cc628869ac15051b0555f18e46a6a7
maat8 <tuaillon.mathieu@gmail.com>
2014-02-18 15:06:10 +0100

    Travis CI update

---
+++ app/config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "randos1000etangs"
+set :domain,      "#{application}.fr"
+set :deploy_to,   "/srv/www/#{domain}"
+set :app_path,    "public_html"
+
+set :repository,  "#{domain}:/var/repos/#{application}.git"
+set :scm,         :git
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
