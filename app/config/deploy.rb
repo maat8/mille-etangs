b10609c3e6b59f86d80b0ee6d66ef063d70a9069
maat8 <tuaillon.mathieu@gmail.com>
2014-02-18 17:51:47 +0100

    Capifony config

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,9 +1,10 @@
-set :application, "randos.maat8"
-set :domain,      "#{application}.fr"
-set :deploy_to,   "/srv/www/#{domain}"
-set :app_path,    "public_html"
+set :application, "randos.maat8.fr"
+set :domain,      "maat8.fr"
+set :deploy_to,   "/srv/www/#{application}"
+set :app_path,    "app"
 
 set :repository,  "https://github.com/maat8/mille-etangs.git"
+set :branch, "develop"
 set :scm,         :git
 
 set :model_manager, "doctrine"
@@ -11,14 +12,20 @@ set :model_manager, "doctrine"
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
-set  :keep_releases,  3
-
-set :shared_files,      ["app/config/parameters.yml"]
-set :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :user, "capifony"
+default_run_options[:pty] = true
+set :use_sudo, false
+set :webserver_user, "www-data"
 
 set :use_composer, true
+set :update_vendors, true
 
-# Be more verbose by uncommenting the following line
-logger.level = Logger::MAX_LEVEL
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
 
+set :writable_dirs, ["app/cache", "app/logs"]
+
+set :keep_releases,  3
 #after "deploy", "deploy:cleanup"
+
+#logger.level = Logger::MAX_LEVEL
