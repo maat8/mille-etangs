e71a512f458116fb7529121c850c570c47948d12
maat8 <tuaillon.mathieu@gmail.com>
2014-02-24 15:26:10 +0100

    Refactoring routing and controllers

--- src/MilleEtangs/RandonneesBundle/Controller/DefaultController.php
+++ src/MilleEtangs/RandonneesBundle/Controller/DefaultController.php
@@ -20,38 +20,6 @@ class DefaultController extends Controller
         ));
     }
 
-    public function itineariesAction($type = null)
-    {
-        $repository = $this->get('doctrine_mongodb')
-            ->getRepository('MilleEtangsRandonneesBundle:Itineary');
-
-        switch($type){
-            case "vtt":
-                $itinearies = $repository->findAllByType("bike");
-                break;
-            case "marche":
-                $itinearies = $repository->findAllByType("hike");
-                break;
-            default:
-                $itinearies = $repository->findAll();
-        }
-
-        return $this->render("MilleEtangsRandonneesBundle:Default:itinearies.html.twig", array(
-            'itinearies' => $itinearies
-        ));
-    }
-
-    public function itinearyAction($slug)
-    {
-        $itineary = $this->get('doctrine_mongodb')
-            ->getRepository('MilleEtangsRandonneesBundle:Itineary')
-            ->findOneBySlug($slug);
-
-        return $this->render('MilleEtangsRandonneesBundle:Default:itineary.html.twig', array(
-            'itineary' => $itineary
-        ));
-    }
-
     public function articlesAction()
     {
         $articles = $this->get('doctrine_mongodb')
@@ -130,64 +98,5 @@ class DefaultController extends Controller
         $response->headers->set('Content-Type', "application/rss+xml");
         
         return $response;
-    }
-
-    public function renderImageAction($id = null)
-    {
-        if (!is_null($id)) {
-            $image = $this->get('doctrine_mongodb')
-                ->getRepository('MilleEtangsRandonneesBundle:Image')
-                ->findOneById($id);
-
-            if (!is_null($image)) {
-                $response = new Response();
-                $response->headers->set('Content-Type', $image->getMimeType());
-                $response->setContent($image->getFile()->getBytes());
-                
-                return $response;
-            }
-        }
-
-        return new Response('Not Found', 404);
-    }
-
-    public function downloadGpxAction($id = null)
-    {
-        if (!is_null($id)) {
-            $itineary = $this->get('doctrine_mongodb')
-                ->getRepository('MilleEtangsRandonneesBundle:Itineary')
-                ->findOneById($id);
-
-            if (!is_null($itineary)) {
-                $response = new Response();
-                $response->headers->set('Content-Type', "application/gpx+xml");
-                $response->headers->set('Content-Disposition', 'attachment; filename="'.$itineary->getName().'.gpx"');
-                $response->sendHeaders();
-                $response->setContent($itineary->getGpx()->getBytes());
-
-                return $response;
-            }
-        }
-
-        return new Response('Not Found', 404);
-    }
-
-    public function renderKmlAction($id = null)
-    {
-        if (!is_null($id)) {
-            $itineary = $this->get('doctrine_mongodb')
-                ->getRepository('MilleEtangsRandonneesBundle:Itineary')
-                ->findOneById($id);
-
-            if (!is_null($itineary)) {
-                $response = new Response();
-                $response->headers->set('Content-Type', "application/vnd.google-earth.kml+xml");
-                $response->setContent($itineary->getKml()->getBytes());
-                
-                return $response;
-            }
-        }
-
-        return new Response('Not Found', 404);
     }
 }
---
+++ src/MilleEtangs/RandonneesBundle/Controller/ItineariesController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace MilleEtangs\RandonneesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use MilleEtangs\RandonneesBundle\Entity\Parcours;
+
+class ItineariesController extends Controller
+{
+    public function itineariesAction($type = null)
+    {
+        $repository = $this->get('doctrine_mongodb')
+            ->getRepository('MilleEtangsRandonneesBundle:Itineary');
+
+        switch($type){
+            case "vtt":
+                $itinearies = $repository->findAllByType("bike");
+                break;
+            case "marche":
+                $itinearies = $repository->findAllByType("hike");
+                break;
+            default:
+                $itinearies = $repository->findAll();
+        }
+
+        return $this->render("MilleEtangsRandonneesBundle:Default:itinearies.html.twig", array(
+            'itinearies' => $itinearies
+        ));
+    }
+
+    public function itinearyAction($slug)
+    {
+        $itineary = $this->get('doctrine_mongodb')
+            ->getRepository('MilleEtangsRandonneesBundle:Itineary')
+            ->findOneBySlug($slug);
+
+        return $this->render('MilleEtangsRandonneesBundle:Default:itineary.html.twig', array(
+            'itineary' => $itineary
+        ));
+    }
+
+    public function renderImageAction($id = null)
+    {
+        if (!is_null($id)) {
+            $image = $this->get('doctrine_mongodb')
+                ->getRepository('MilleEtangsRandonneesBundle:Image')
+                ->findOneById($id);
+
+            if (!is_null($image)) {
+                $response = new Response();
+                $response->headers->set('Content-Type', $image->getMimeType());
+                $response->setContent($image->getFile()->getBytes());
+                
+                return $response;
+            }
+        }
+
+        return new Response('Not Found', 404);
+    }
+
+    public function downloadGpxAction($id = null)
+    {
+        if (!is_null($id)) {
+            $itineary = $this->get('doctrine_mongodb')
+                ->getRepository('MilleEtangsRandonneesBundle:Itineary')
+                ->findOneById($id);
+
+            if (!is_null($itineary)) {
+                $response = new Response();
+                $response->headers->set('Content-Type', "application/gpx+xml");
+                $response->headers->set('Content-Disposition', 'attachment; filename="'.$itineary->getName().'.gpx"');
+                $response->sendHeaders();
+                $response->setContent($itineary->getGpx()->getBytes());
+
+                return $response;
+            }
+        }
+
+        return new Response('Not Found', 404);
+    }
+
+    public function renderKmlAction($id = null)
+    {
+        if (!is_null($id)) {
+            $itineary = $this->get('doctrine_mongodb')
+                ->getRepository('MilleEtangsRandonneesBundle:Itineary')
+                ->findOneById($id);
+
+            if (!is_null($itineary)) {
+                $response = new Response();
+                $response->headers->set('Content-Type', "application/vnd.google-earth.kml+xml");
+                $response->setContent($itineary->getKml()->getBytes());
+                
+                return $response;
+            }
+        }
+
+        return new Response('Not Found', 404);
+    }
+}
