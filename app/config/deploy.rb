c262dd8d8a263320fba86c4c6798bb36401b46b2
maat8 <tuaillon.mathieu@gmail.com>
2014-05-21 21:34:31 -0400

    Capifony multistaging conf

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -1,41 +1,4 @@
-set :application, "randos.maat8.fr"
-set :domain,      "maat8.fr"
-set :deploy_to,   "/srv/www/#{application}"
-set :app_path,    "app"
-
-set :repository,  "https://github.com/maat8/mille-etangs.git"
-set :branch, "develop"
-set :scm,         :git
-
-set :model_manager, "doctrine"
-
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain, :primary => true       # This may be the same as your `Web` server
-
-set :user, "capifony"
-default_run_options[:pty] = true
-set :use_sudo, false
-set :webserver_user, "www-data"
-
-set :use_composer, true
-set :update_vendors, true
-set :dump_assetic_assets, true
-
-set :shared_files, ["app/config/parameters.yml"]
-set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
-
-set :writable_dirs, ["app/cache", "app/logs"]
-
-set :keep_releases,  3
-after "deploy", "deploy:cleanup"
-
-# perform tasks after deploying
-after "deploy" do
-  # clear the cache
-  run "cd /srv/www/randos.maat8.fr/current && php app/console cache:clear"
-
-  # dump assets (if using assetic)
-  run "cd /srv/www/randos.maat8.fr/current && php app/console assetic:dump"
-end
-
-logger.level = Logger::MAX_LEVEL
+set :stages,        %w(production staging)
+set :default_stage, "staging"
+set :stage_dir,     "app/config"
+require 'capistrano/ext/multistage'
---
+++ app/config/production.rb
@@ -0,0 +1,41 @@
+set :application, "randos1000etangs.fr"
+set :domain,      "maat8.fr"
+set :deploy_to,   "/srv/www/#{application}"
+set :app_path,    "app"
+
+set :repository,  "https://github.com/maat8/mille-etangs.git"
+set :branch, "master"
+set :scm,         :git
+
+set :model_manager, "doctrine"
+
+role :web,        domain
+role :app,        domain, :primary => true
+
+set :user, "capifony"
+default_run_options[:pty] = true
+set :use_sudo, false
+set :webserver_user, "www-data"
+
+set :use_composer, true
+set :update_vendors, true
+set :dump_assetic_assets, true
+
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [app_path + "/logs", web_path + "/uploads", "vendor"]
+
+set :writable_dirs, ["app/cache", "app/logs"]
+
+set :keep_releases,  3
+after "deploy", "deploy:cleanup"
+
+# perform tasks after deploying
+after "deploy" do
+  # clear the cache
+  #run "cd /srv/www/randos.maat8.fr/current && php app/console cache:clear"
+
+  # dump assets (if using assetic)
+  #run "cd /srv/www/randos.maat8.fr/current && php app/console assetic:dump"
+end
+
+logger.level = Logger::MAX_LEVEL
